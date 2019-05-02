5f41eebc63363229fc57d031933f3c73d2c62c59
Gabroel <gabriel19siqueira@gmail.com>
2019-05-01 23:25:44 -0300

    Create test_stant class

---
+++ src/project_stant/stant_test.java
@@ -0,0 +1,89 @@
+package project_stant;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class stant_test {
+
+	public static void main(String[] args) {
+
+		String fileName = "C:\\proposals.txt";
+
+		File arq = new File(fileName);
+
+		System.out.println("Organization of lectures\n");
+		System.out.println("Track A:\n");
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader fileBuffer = new BufferedReader(fileReader);
+
+			String line = fileBuffer.readLine();
+
+			int time = 0;
+			int lineTime;
+
+			while (line != null) {
+
+				if (line.matches("(.*)lightning(.*)")) {
+					lineTime = Integer.parseInt(line.replaceAll("(.*)lightning(.*)", "5"));
+					time += lineTime;
+					// System.out.println(lineTime);
+					// System.out.println(time);
+				} else {
+					lineTime = Integer.parseInt(line.replaceAll("[^0-9]+", ""));
+					time += lineTime;
+					// System.out.println(lineTime);
+					// System.out.println(time);
+
+					System.out.println(line);
+				}
+
+				if (time == 180) {
+					System.out.println("Almoço");
+				}
+
+				// System.out.printf("%s\n", line + "\n");
+				// int firstTime = Integer.parseInt(line.replaceAll("[^0-9]+",
+				// ""));
+				// System.out.println(firstTime);
+				line = fileBuffer.readLine();
+
+				// if (firstAccountant == 4) {
+				// System.out.println("Almoço\n");
+
+				// }
+
+			}
+
+			System.out.println("Track B:\n");
+
+			// int secondAccountant = 11;
+			// while (line != null && secondAccountant < 21) {
+			// System.out.printf("%s\n", line + "\n");
+			// int secondTime = Integer.parseInt(linha.replaceAll("[^0-9]+",
+			// " "));
+			// System.out.println(secondTime);
+			// line = fileBuffer.readLine();
+
+			// secondAccountant++;
+
+			// if (secondAccountant == 15) {
+			// System.out.println("Almoço\n");
+
+			// }
+
+			// }
+
+			fileBuffer.close();
+
+		} catch (IOException e) {
+			System.err.printf("Error opening file: %s.\n", e.getMessage());
+
+		}
+
+	}
+
+}
